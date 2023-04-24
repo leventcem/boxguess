055faedc43c6496704b1a1aa77a1ae79bdcb9fa1
NikitaVyas12994 <90673852+NikitaVyas12994@users.noreply.github.com>
2023-04-24 14:22:48 +0530

    Update Stats.jsx

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -57,6 +57,10 @@ const LeaderBoard = () => {
                 <tr className=" " >
                   <td className="text-center  p-1 md:p-3">6 Box - </td>
                   <td className="text-center  p-1 md:p-3">{stats?.totalOf6Boxes || 0}</td>
+                </tr>    
+                <tr className=" " >
+                  <td className="text-center  p-1 md:p-3">8 Box - </td>
+                  <td className="text-center  p-1 md:p-3">{stats?.totalOf8Boxes || 0}</td>
                 </tr>                     
                 <tr className=" " >
                   <td className="text-center  p-1 md:p-3">12 Box - </td>
